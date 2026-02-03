3f847536edb704fb0c1116c0188f3097d16ef7bf
HongTaeHun337 <ghdxogns12253@naver.com>
2026-02-03 17:54:28 +0900

    0203수업

--- ex32_transaction.sql
+++ ex32_transaction.sql
@@ -50,26 +50,168 @@ rollback;
 delete from tblUser;
 
 
+--1주일
+
+
+
+--------------------------------------------------------------------------------
+
+-- 트랜잭션은 딱 1개 존재
+
+-- 트랜잭션 시작
+
+select * from tblTrans; --19명, select 미포함
+
+insert into tblTrans values ('강아지', '개발부', '사원');
+
+update tblTrans set jikwi = '이사' where name = '홍길동';
+
+delete from tblTrans where name = '한석봉';
+
+--트랜잭션 단위로 처리(일부 SQL만 따로 처리 불가능!!)
+select * from tblTrans; --현재 세션에만 반영
+
+rollback;
+
+select * from tblTrans;
+
+--commit;
+
+/*
+    
+    ***** 트랜잭션이 언제 시작해서 ~ 언제 끝나는지?
+    
+    새로운 트랜잭션이 시작하는 경우
+    1. 클라이언트가 접속한 직후
+    2. commit 실행 직후
+    3. rollback 실행 직후
+    
+    현재 트랜잭션이 끝나는 경우
+    1. 클라이언트 접속 종료
+        a. 정상 종료
+            - 현재 트랜잭샨이 모두 완료 > 아무일도 안 일어남
+            - 현재 트랜잭션이 반영이 안된 상태 > 질문?
+        b. 비정상 종료
+        
+    2. commit 실행 직후
+    3. rollback 실행 직후
+    4. DDL(구조) 실행 직전 > 자동 Commit
+        -create, alter, drop
+        
+
+*/
+
+commit;
+
+insert into tblTrans values ('강아지', '개발부', '사원');
+
+select * from tblTrans;
+
+insert into tblTrans values ('강아지', '개발부', '사원');
+
+update tblTrans set jikwi = '이사' where name = '홍길동';
+
+-- 옆사람 > 시퀀스 객체 생성 요청
+
+create sequence seqAAA;
+
+rollback;
+
+select * from tblTrans;
+
+delete from tblTrans where name = '한석봉';
+
+------------------------------------------------------------
+
+-- savepoint
+commit;
+
+insert into tblTrans values ('고양이', '개발부', '사원');
+
+savepoint a;
+
+update tblTrans set jikwi = '사장' where name = '홍길동';
+
+savepoint b;
+
+delete from tblTrans where name = '한석봉';
+
+SELECT * FROM tblTrans;
+
+rollback to b;
+
+SELECT * FROM tblTrans;
+
+rollback to a;
+
+SELECT * FROM tblTrans;
+
+rollback;
+
+SELECT * FROM tblTrans;
+
+
+----------------------------------------------------
+
+
+/*
+
+    오라클 서버
+    - 팀원A: SQL Developer + HR > 접속
+    - 팀원B: DBeaver + HR > 접속
+    - 팀원C: DataGrip + HR > 접속
+    
+    팀 작업 시 > 트랜잭션(***) & 툴 특성(Auto Commit 기능 On/Off)
+    
+    팀 작업
+    1. 각자 DB 사용 > 합치는 방법
+    2. 하나 DB 사용 > 실시간 방법
+    
+    5명 > 그중 1명 PC > 오라클 서버 선정
+    1. 프로젝트 계정 생성
+    2. 방화벽 오픈(오라클 사용하는 포트) > 오라클 서버 컴퓨터에서 설정
+    
+    집
+    1. *.sql > 관리 > 6~7
+    2. 외부 오라클 사용 > 3~4
+        a. 클라우드 서비스 > AWS(X), OracleCloud(O - 팀작업 살짝 버벅임)
+        b. 집 > PC(24H) + 공유기(포트 포워딩) > 검색
+    
+    
+*/
+commit;
+
+insert into tblTrans values ('병아리', '개발부', '사원');
+
+SELECT * FROM tblTrans;
+
+commit;
+
+SELECT * FROM tblTrans;
 
 
 
 
 
+    
+-- 프로젝트 계정 생성하기(12c 이후 > 계정명 앞에 'C##')
+create user 계정명 identified by 암호;
+
+-- 관리자만 가능
+
+
+
+show user;
+alter session set "_ORACLE_SCRIPT"=true;
+create user team identified by java1234;
+grant connect, resource, dba to team; --DCL
+alter User team default tablespace users;
 
 
 
 
+/*
 
+    프로젝트
 
-
-
-
-
-
-
-
-
-
-
-
-
+*/
